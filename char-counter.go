33fd5795f5635124694c1bec0af706d4b652ef7b
Muhammad Fajar Baihaqi <45395118+glizzard27@users.noreply.github.com>
2022-12-19 21:02:46 +0700

    Add files via upload

---
+++ char-counter.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Meminta input dari pengguna
+	var sentence string
+	fmt.Println("-------------------------------------------")
+	fmt.Print("Masukkan kalimat (Tanpa spasi): ")
+	fmt.Scanf("%s", &sentence)
+	fmt.Println("-------------------------------------------")
+
+	// Menghitung banyaknya masing-masing karakter dalam kalimat
+	charCount := make(map[rune]int)
+	for _, char := range sentence {
+		charCount[char]++
+	}
+
+	// Menampilkan hasilnya
+	for char, count := range charCount {
+		fmt.Printf("Jumlah karakter '%c': %d\n", char, count)
+	}
+	fmt.Println("-------------------------------------------")
+}
